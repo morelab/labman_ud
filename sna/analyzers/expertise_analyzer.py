b5d5cb26de0053db80e54f59ebf7b02150cbab80
Aitor Almeida <aitoralmeida@gmail.com>
2013-07-02 11:48:12 +0200

    added desktop version queries. Change them to django asap.

--- sna/analyzers/expertise_analyzer.py
+++ sna/analyzers/expertise_analyzer.py
@@ -5,6 +5,7 @@ Created on Tue Jul 02 11:15:32 2013
 @author: aitor
 """
 from sets import Set
+import pg # TODO: delete once the queries are djangoized
 
 
 class expertise_analyzer():
@@ -67,3 +68,90 @@ class expertise_analyzer():
                     expertises[tag] = Set()
                 expertises[tag].add(person)
         return expertises
+        
+    # ************************************************************************************    
+    # TODO: All this functions must be djangoized. I'm not touching the queries until
+    # then, they do not work rigth now, only for "documentation" purposes
+    # ************************************************************************************ 
+        
+    def query_persons_tags():
+        query_persons_tags = ("select persons_person.slug, utils_tag.slug "
+                    "from projects_assignedperson, persons_person, projects_project, utils_tag, projects_projecttag "
+                    "where projects_assignedperson.person_id= persons_person.id "
+                    "and projects_assignedperson.project_id = projects_project.id "
+                    "and projects_assignedperson.role_id <> 3 "
+                    "and projects_projecttag.project_id = projects_project.id "
+                    "and projects_projecttag.tag_id = utils_tag.id "
+                    "order by projects_project.id")
+                
+        con = pg.connect(dbname='labman', host='localhost', user='meh')
+        res = con.query(query_persons_tags)
+        con.close()
+        return res.getresult()
+    
+    def query_person_tags(personSlug):
+        query_person_tags = ("select utils_tag.slug "
+                    "from projects_assignedperson, persons_person, projects_project, utils_tag, projects_projecttag "
+                    "where projects_assignedperson.person_id= persons_person.id "
+                    "and projects_assignedperson.project_id = projects_project.id "
+                    "and projects_assignedperson.role_id <> 3 "
+                    "and projects_projecttag.project_id = projects_project.id "
+                    "and projects_projecttag.tag_id = utils_tag.id "
+                    "and persons_person.slug = $1 " )
+                
+        con = pg.connect(dbname='labman', host='localhost', user='meh')
+        res = con.query(query_person_tags, (personSlug, ))
+        con.close()
+        return res.getresult()
+        
+    def query_expertise_person_year(expertise, person, year):
+        query_expertise_person_year = ("select count(projects_project.id) "
+                        "from projects_project, projects_assignedperson, persons_person, projects_projecttag, utils_tag "
+                        "where projects_project.start_year <= {0} "
+                        "and projects_project.end_year >= {0} "
+                        "and persons_person.slug = '{1}' "
+                        "and utils_tag.slug = '{2}' "
+                        "and utils_tag.id = projects_projecttag.tag_id "
+                        "and projects_project.id = projects_projecttag.project_id "
+                        "and projects_project.id = projects_assignedperson.project_id "
+                        "and persons_person.id = projects_assignedperson.person_id ").format(year,person,expertise)
+                    
+        con = pg.connect(dbname='labman', host='localhost', user='meh')
+        res = con.query(query_expertise_person_year)
+        con.close()
+        return res.getresult()
+        
+    # getting the results from query_person_tags
+    def get_user_tags(results):
+        tags = {}
+        for r in results:
+            name = r[0]
+            if not name in tags.keys():
+                tags[name] = Set()
+            tags[name].add(r[1])
+        return tags
+        
+    def query_person_slugs():
+        query_slugs = ("select distinct persons_person.slug "
+                    "from persons_person")
+                
+        con = pg.connect(dbname='labman', host='localhost', user='meh')
+        res = con.query(query_slugs)
+        con.close()
+        return res.getresult()
+    
+    def add_tags(self, slugs):
+        persons_tags = {}
+        for s in slugs:
+            name = s[0]
+            tags = self.query_person_tags(name)
+            tag_values = {}
+            for t in tags:
+                tag = t[0]
+                if not tag in tag_values.keys():
+                    tag_values[tag] = 1
+                else:
+                    tag_values[tag] += 1
+            persons_tags[name] = tag_values
+        
+        return persons_tags
