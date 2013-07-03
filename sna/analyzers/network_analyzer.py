69b9edc66332b31cdbcac02e90055715716c0bb8
Aitor Almeida <aitoralmeida@gmail.com>
2013-07-03 12:49:14 +0200

    Added basic community detection methods

--- sna/analyzers/network_analyzer.py
+++ sna/analyzers/network_analyzer.py
@@ -13,7 +13,8 @@ class network_analyzer():
     def load_edgelist(self, edgelist): 
         G = nx.parse_edgelist(edgelist)
         return G
-        
+    
+    # Centralities    
     def calculate_degree_centrality(G):
         cent_degree = nx.degree_centrality(G)
         sorted_cent_degree = OrderedDict(sorted(cent_degree.items(), key=lambda t: t[1], reverse=True))
@@ -38,3 +39,20 @@ class network_analyzer():
         page_rank = nx.pagerank(G)
         sorted_page_rank = OrderedDict(sorted(page_rank.items(), key=lambda t: t[1], reverse=True))
         return sorted_page_rank
+        
+    # Communities
+    def calculate_cliques(G):
+        cliques = list(nx.find_cliques(G))
+        return cliques
+        
+    def calculate_main_k_core(G):
+        core_main = nx.k_core(G)
+        return core_main        
+        
+    def calculate_k_core(G, K):
+        core_k = nx.k_core(G, k=K)
+        return core_k
+        
+    def calculate_k_clique(G, K):
+        communities = nx.k_clique_communities(G, K)
+        return communities
