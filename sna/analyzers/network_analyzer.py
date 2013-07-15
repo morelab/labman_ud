617f21f9a3b155180780bbc1f3c70811d13a611c
Aitor Almeida <aitoralmeida@gmail.com>
2013-07-15 11:10:24 +0200

    redone the analysis

--- sna/analyzers/network_analyzer.py
+++ sna/analyzers/network_analyzer.py
@@ -6,53 +6,140 @@ Created on Wed Jul 03 09:23:31 2013
 """
 
 import networkx as nx
-from collections import OrderedDict
+import matplotlib.pylab as plt
+from networkx.readwrite import json_graph
+import json
+import binascii
+from networkx.readwrite import gexf
 
-class network_analyzer():
-    
-    def load_edgelist(self, edgelist): 
-        G = nx.parse_edgelist(edgelist)
-        return G
-    
-    # Centralities    
-    def calculate_degree_centrality(G):
-        cent_degree = nx.degree_centrality(G)
-        sorted_cent_degree = OrderedDict(sorted(cent_degree.items(), key=lambda t: t[1], reverse=True))
-        return sorted_cent_degree
-        
-    def calculate_betweenness_centrality(G):    
-        cent_betweenness = nx.betweenness_centrality(G)
-        sorted_cent_betweenness = OrderedDict(sorted(cent_betweenness.items(), key=lambda t: t[1], reverse=True))
-        return sorted_cent_betweenness
-        
-    def calculate_closeness_centrality(G):   
-        cent_closeness = nx.closeness_centrality(G)
-        sorted_cent_closeness = OrderedDict(sorted(cent_closeness.items(), key=lambda t: t[1], reverse=True))
-        return sorted_cent_closeness
+show_graphs = False
 
-    def calculate_eigenvector_centrality(G):   
-        cent_eigenvector = nx.eigenvector_centrality(G)
-        sorted_cent_eigenvector = OrderedDict(sorted(cent_eigenvector.items(), key=lambda t: t[1], reverse=True))
-        return sorted_cent_eigenvector
-            
-    def calculate_pagerank(G):
-        page_rank = nx.pagerank(G)
-        sorted_page_rank = OrderedDict(sorted(page_rank.items(), key=lambda t: t[1], reverse=True))
-        return sorted_page_rank
+def load_graph(file_path, is_directed=False):
+    if is_directed:
+        G = nx.read_edgelist(path=file_path, delimiter=';', create_using= nx.DiGraph())
+    else:
+        G = nx.read_edgelist(path=file_path, delimiter=';')
+    return G
+
+def show_info(G):
+    print 'Is directed?: %s' %(str(G.is_directed())) 
+    print 'Nodes: %i' % (len(G.nodes()))
+    print 'Edges: %i' % (len(G.edges()))
+    
+def export_json(file_path, G):
+    data = json_graph.node_link_data(G)
+    json.dump(data, open(file_path, 'w'))
+
+def anonimize_json(input_file, output_file, G):
+    f = open(input_file, 'r')
+    file_text = f.read()
+    f.close()
+    for node in G.nodes():
+        anon_name = str(binascii.crc32(node))
+        file_text = file_text.replace(node, anon_name)
         
-    # Communities
-    def calculate_cliques(G):
+    f = open(output_file, 'w')
+    f.write(file_text)
+    f.flush()
+    f.close()
+    
+def filter_pairs(G):
+    if G.is_directed:
+        filtered_G = nx.DiGraph()
+        components = nx.weakly_connected_component_subgraphs(G)
+    else:
+        filtered_G = nx.Graph()
+        components = nx.connected_component_subgraphs(G)
+        
+    for cc in components: 
+        if len(cc.nodes()) > 2:
+            filtered_G.add_nodes_from(cc)
+            filtered_G.add_edges_from(cc.edges())  
+    
+    return filtered_G
+    
+    
+def analyze_graph(G):    
+    components = []    
+    if not G.is_directed():
+        components = nx.connected_component_subgraphs(G)
+    else:
+        components = nx.weakly_connected_component_subgraphs(G)
+    
+    i = 0
+    
+    for cc in components:            
+        #Set the connected component for each group
+        for node in cc:
+            G.node[node]['component'] = i
+        
+        if show_graphs:
+            nx.draw(cc)
+            plt.show()
+      
+        #Calculate the in component betweeness, closeness and eigenvector centralities        
+        cent_betweenness = nx.betweenness_centrality(cc)              
+        cent_eigenvector = nx.eigenvector_centrality_numpy(cc)
+        cent_closeness = nx.closeness_centrality(cc)
+        
+        for name in cc.nodes():
+            G.node[name]['cc-betweenness'] = cent_betweenness[name]
+            G.node[name]['cc-eigenvector'] = cent_eigenvector[name]
+            G.node[name]['cc-closeness'] = cent_closeness[name]
+        
+        i +=1
+               
+    if not G.is_directed():
+        # Calculate cliques
         cliques = list(nx.find_cliques(G))
-        return cliques
+        j = 0
+        processed_members = []
+        for clique in cliques:
+            for member in clique:
+                if not member in processed_members:
+                    G.node[member]['cliques'] = []
+                    processed_members.append(member)
+                G.node[member]['cliques'].append(j)
+            j +=1
         
-    def calculate_main_k_core(G):
-        core_main = nx.k_core(G)
-        return core_main        
+        #calculate degree    
+        degrees = G.degree()
+        for name in degrees:
+            G.node[name]['degree'] = degrees[name]
+       
+    else: #is_directed
+        out_degrees = G.out_degree()
+        in_degrees = G.in_degree()
         
-    def calculate_k_core(G, K):
-        core_k = nx.k_core(G, k=K)
-        return core_k
+        for name in G.nodes():
+            G.node[name]['out_degree'] = out_degrees[name]
+            G.node[name]['in_degree'] = in_degrees[name]
         
-    def calculate_k_clique(G, K):
-        communities = nx.k_clique_communities(G, K)
-        return communities
+    betweenness = nx.betweenness_centrality(G)
+    eigenvector = nx.eigenvector_centrality_numpy(G)
+    closeness = nx.closeness_centrality(G)
+    pagerank = nx.pagerank(G)
+    
+    for name in G.nodes():
+        G.node[name]['betweenness'] = betweenness[name]
+        G.node[name]['eigenvector'] = eigenvector[name]
+        G.node[name]['closeness'] = closeness[name]
+        G.node[name]['pagerank'] = pagerank[name]
+
+        
+    return G
+
+#just for testing    
+if __name__ == '__main__':
+    print 'Loading graph'
+    G = load_graph('./data/expertiseUndirected.csv', True)
+    show_info(G)
+    print 'filtering pairs'
+    G = filter_pairs(G)
+    show_info(G)
+    G = analyze_graph(G)
+    print G.nodes(data=True)
+    export_json('./data/expertise-undirected.json', G)
+    if G.is_directed():
+        gexf.write_gexf(G, './data/expertise-undirected.gexf')
+    print '************FIN*************'        
